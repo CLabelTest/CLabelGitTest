13e286897d97388fafa306e6e9bd0a9803fc910b
dmudge <davem@consolidatedlabel.com>
2014-05-08 16:27:16 -0400

    Test 2

--- TestLoad/default.aspx.vb
+++ TestLoad/default.aspx.vb
@@ -2,6 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim i As Integer
+        i = 1 / 0
 
     End Sub
 
